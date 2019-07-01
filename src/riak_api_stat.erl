f904ed1564f4122dbefecc7b004fe8b2b2010057
banana <savannah.allsop@bet365.com>
2019-07-01 16:54:48 +0100

    reorganised

--- src/riak_api_stat.erl
+++ src/riak_api_stat.erl
@@ -37,7 +37,7 @@
 
 -define(SERVER, ?MODULE).
 -define(APP, riak_api).
--define(PFX, riak_stat_admin:prefix()).
+-define(PFX, riak_stat:prefix()).
 
 %% -------------------------------------------------------------------
 %% API
@@ -47,7 +47,7 @@ start_link() ->
   gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
 register_stats() ->
-  riak_stat_coordinator:coordinate(admin, {register, {?APP, stats()}}).
+  riak_stat:register(?APP, stats()).
 
 %% @doc Return current aggregation of all stats.
 -spec get_stats() -> proplists:proplist().
@@ -59,7 +59,7 @@ produce_stats() ->
   {?APP, get_values([?APP])}.
 
 get_values(Path) ->
-  riak_stat_mngr:get_values(Path).
+  riak_stat:get_app_stats(Path).
 
 update(Arg) ->
   gen_server:cast(?SERVER, {update, Arg}).
@@ -91,8 +91,7 @@ code_change(_OldVsn, State, _Extra) ->
 %% @doc Update the given `Stat'.
 -spec update1(term()) -> ok.
 update1(pbc_connect) ->
-  riak_stat_coordinator:coordinate(exometer,
-    {update, {[?PFX, ?APP, pbc_connects], 1, spiral}}).
+  riak_stat:update([?PFX, ?APP, pbc_connects], 1, spiral).
 
 %% -------------------------------------------------------------------
 %% Private
